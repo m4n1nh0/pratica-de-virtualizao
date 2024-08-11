07964b18ed14cb367216b074c6e1402b14d67849
Mariano F Mendonca <marianofmendonca@gmail.com>
2024-08-10 22:18:21 -0300

    Ajuste de virtualização

--- projects/backend/app.js
+++ projects/backend/app.js
@@ -8,7 +8,8 @@ const db = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT || 3306
 });
 
 // Conectar ao banco de dados
